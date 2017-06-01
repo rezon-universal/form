57a1252ab940f06d3c295beece168e85553bcb79
Oles <boiaryn.o@gmail.com>
2017-06-01 11:32:11 +0300

    no message

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,14 +15,7 @@ module.exports = function(grunt) {
 			all: {
 				'pre-commit': 'pre-commit'
 			}
-		},
-
-		eslint: {
-			options: {
-				quiet: true
-			},
-			target: ['src/*.js']
-		},
+		},	
 
 		mocha: {
 			all: {
@@ -55,8 +48,7 @@ module.exports = function(grunt) {
 
 	// Load tasks
 	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-mocha');
-	grunt.loadNpmTasks('grunt-eslint');
+	grunt.loadNpmTasks('grunt-mocha');	
 	grunt.loadNpmTasks('grunt-githooks');
 
 	// Default task.
