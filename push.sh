e8c3089968ecca1713bfcde357713f7c54027a80
BoyarinO <boiaryn.o@gmail.com>
2017-06-02 14:17:33 +0300

    no message

--- push.sh
+++ push.sh
@@ -15,8 +15,9 @@ commit_website_files() {
 }
 
  upload_files() {
-   git remote add origin-pages https://${GH_TOKEN}@github.com/rezon-universal/form.git > /dev/null 2>&1
-   git push --quiet --set-upstream origin-pages minify 
+   git remote add rezon https://${GH_TOKEN}@github.com/rezon-universal/form.git > /dev/null 2>&1
+   git fetch rezon
+   git push --quiet --set-upstream rezon minify 
  }
 
 setup_git
