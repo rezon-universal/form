cbc27352b40b118872a5c42773b51ff21e810841
BoyarinO <boiaryn.o@gmail.com>
2017-06-02 17:52:44 +0300

    no message

--- push.sh
+++ push.sh
@@ -12,7 +12,7 @@ commit_website_files() {
 
  upload_files() {
    git remote add rezon https://${GH_TOKEN}@github.com/rezon-universal/form.git > /dev/null 2>&1   
-   git fetch rezon master
+   git fetch rezon 
    git merge rezon/master
    git add dest/\*.min.js dest/\*min.css
    git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
