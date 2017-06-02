2ae1d85b470bf2623ab6e146221fe35c731a70d1
travisbotik <tickets_mail@mail.ua>
2017-06-02 11:15:58 +0300

    no message

--- push.sh
+++ push.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
 setup_git() {
-  echo "*.js"	
+  echo "rezon.form.js"	
   git config --global user.email "tickets_mail@mail.ua"
   git config --global user.name "travisbotik"
 }
 
-commit_website_files() {
+# commit_website_files() {
 
-  # git checkout -b gh-pages
-  # git add . *.js
-  # git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-}
+  # # git checkout -b gh-pages
+  # # git add . *.js
+  # # git commit --message "travis build: $travis_build_number"
+# }
 
-upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/rezon-universal/form.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-pages gh-pages 
-}
+# upload_files() {
+  # git remote add origin-pages https://${gh_token}@github.com/rezon-universal/form.git > /dev/null 2>&1
+  # git push --quiet --set-upstream origin-pages gh-pages 
+# }
 
 setup_git
-commit_website_files
-upload_files
+#commit_website_files
+#upload_files
