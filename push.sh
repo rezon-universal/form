eb7e99ce5134e40c747a023ae9beebfd288b02e4
travisbotik <tickets_mail@mail.ua>
2017-06-02 11:40:36 +0300

    no message

--- push.sh
+++ push.sh
@@ -16,7 +16,7 @@ commit_website_files() {
 
  upload_files() {
    git remote add origin-pages https://${GH_TOKEN}@github.com/rezon-universal/form.git > /dev/null 2>&1
-   git push --quiet --set-upstream origin-pages gh-minify 
+   git push --quiet --set-upstream origin-pages minify 
  }
 
 setup_git
