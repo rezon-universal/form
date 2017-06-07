01897a0552372b6fbb082e2e0e1c90e4a136b2de
BoyarinO <boiaryn.o@gmail.com>
2017-06-07 11:22:50 +0300

    no message

--- push.sh
+++ push.sh
@@ -10,14 +10,14 @@ commit_website_files() {
   git checkout --orphan minify
   git rm -rf . 
   git add dest/\*.min.js dest/\*min.css
-  git commit --message "Travis minify: $TRAVIS_BUILD_NUMBER"
-  
+  git commit --message "Travis minify: $TRAVIS_BUILD_NUMBER"  
 }
  upload_files() {
    git remote add rezon https://${GH_TOKEN}@github.com/rezon-universal/form.git > /dev/null 2>&1      
    git fetch rezon
    git checkout master     
-   git merge minify --no-commit   
+   git merge minify --no-commit
+   git commit -a -m "Travis build: $TRAVIS_BUILD_NUMBER"
    git push   --quiet --set-upstream rezon master
  }
 
