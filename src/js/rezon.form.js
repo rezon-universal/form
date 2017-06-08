4a8cf517f88bc5d53dd64d872e5c051a3036f550
BoyarinO <boiaryn.o@gmail.com>
2017-06-08 11:44:33 +0300

    no message

--- src/js/rezon.form.js
+++ src/js/rezon.form.js
@@ -2,5 +2,6 @@
 x=x+1;
 var x=1;
 var z=1;
+var x2=x+z+x;
 console.log('hello');
 /* eslint-enable */
--- src/js/second.js
+++ src/js/second.js
@@ -5,4 +5,10 @@ var test = function()
 	var z=2;
 	
 }
+var test2 = function()
+{
+	var x=1;
+	var z=2;
+	
+}
 /* eslint-enable */
