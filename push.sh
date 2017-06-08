a468e0cbae9faa969f2d51b7638d0f336893d288
BoyarinO <boiaryn.o@gmail.com>
2017-06-08 11:39:49 +0300

    no message

--- push.sh
+++ push.sh
@@ -12,7 +12,7 @@ commit_website_files() {
   git rev-parse --show-prefix
   echo "-----------"
   git rm -rf .
-  git add dest/\*.min.js dest/\*min.css  dest/origin/*
+  git add dest/\*.min.js dest/\*min.css  dest/src/*
   git commit --message "Travis minify: $TRAVIS_BUILD_NUMBER"  
   echo "----minify files----"
   git ls-tree -r minify
