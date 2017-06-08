256627b22083b45b4be04ac37a963c60241d8b53
BoyarinO <boiaryn.o@gmail.com>
2017-06-08 13:06:59 +0300

    no message

--- push.sh
+++ push.sh
@@ -2,28 +2,22 @@
 
 setup_git() {  
   echo ${USER}	
-  git config --global user.email "tickets_mail@mail.ua"
-  git config --global user.name "travisbotik"
+  git config --global user.email ${GH_EMAIL}
+  git config --global user.name ${GH_USER}
 }
 
 commit_website_files() { 
-  git checkout --orphan minify
-  echo "---curr folder---"
-  git rev-parse --show-prefix
-  echo "-----------"
-  git rm -rf .
+  git checkout --orphan --quiet minify 
+  git rm -rf --quiet .
   git add minified/\*.min.js minified/\*min.css minified/\*.map origin/* html/*
-  git commit --message "Travis minify: $TRAVIS_BUILD_NUMBER"  
-  echo "----minify files----"
-  git ls-tree -r minify
-  echo "--------"  
+  git commit --message "Travis minify: $TRAVIS_BUILD_NUMBER"   
 }
  upload_files() {
    git remote add rezon https://${GH_TOKEN}@github.com/rezon-universal/form.git > /dev/null 2>&1           
-   git fetch rezon   
-   git checkout -b master rezon/master  
-   git merge minify --commit -m "Travis build: $TRAVIS_BUILD_NUMBER" -X theirs   
-   git push rezon master  --set-upstream 
+   git fetch --quiet rezon   
+   git checkout -b --quiet master rezon/master  
+   git merge minify --quiet --commit -m "Travis build: $TRAVIS_BUILD_NUMBER" -X theirs   
+   git push rezon master  --set-upstream --quiet
  }
 
 setup_git
