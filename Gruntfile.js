2e40b0ab85921d2845cc182b61dbb699a47ce10c
Oles <boiaryn.o@gmail.com>
2017-10-05 12:04:35 +0300

    no message

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,92 +1,108 @@
-module.exports = function(grunt) {
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		meta: {
-			banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd HH:MM:ss") %> */'
-		},
+	module.exports = function(grunt) {
+	    grunt.initConfig({
+	        pkg: grunt.file.readJSON('package.json'),
+	        meta: {
+	            banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd HH:MM:ss") %> */'
+	        },
 
-		githooks: {
-			options: {
-				hashbang: '#!/bin/sh',
-				template: 'install/template/shell.hb',
-				startMarker: '# GRUNT-GITHOOKS START',
-				endMarker: '# GRUNT-GITHOOKS END'
-			},
-			all: {
-				'pre-commit': 'pre-commit'
-			}
-		},
+	        githooks: {
+	            options: {
+	                hashbang: '#!/bin/sh',
+	                template: 'install/template/shell.hb',
+	                startMarker: '# GRUNT-GITHOOKS START',
+	                endMarker: '# GRUNT-GITHOOKS END'
+	            },
+	            all: {
+	                'pre-commit': 'pre-commit'
+	            }
+	        },
 
-		eslint: {
-			options: {
-				quiet: true
-			},
-			target: ['src/*.js']
-		},
+	        eslint: {
+	            options: {
+	                quiet: true
+	            },
+	            target: ['src/*.js']
+	        },
 
-		mocha: {
-			all: {
-				src: ['test/*.html'],
-			},
-			options: {
-				run: true
-			}
-		},
-		concat: {			
-			js: {
-				src: 'src/js/*.js',
-				dest: 'js/concat.js'
-			},
-			css: {
-				src: 'src/css/*.css',
-				dest: 'css/concat.css'
-			}
-		},
-		cssmin: {
-			css:{
-				src: 'css/concat.css',
-				dest: 'minified/css/concat.min.css'
-			}
-		},
-		// Minifies JS files
-		uglify: {
-			options: {
-				preserveComments: /^!|@preserve|@license|@cc_on/i,
-				sourceMap: true,
-				footer: '\n'
-			},
-			dist: {
-				files: [{
-					expand:	true,
-					cwd:	'js',
-					src:	'concat.js',
-					dest:	'minified/js',
-					ext:	'.min.js',
-					extDot:	'last'
-				}]
-			}
-		},
+	        mocha: {
+	            all: {
+	                src: ['test/*.html'],
+	            },
+	            options: {
+	                run: true
+	            }
+	        },
+	        concat: {
+	            js: {
+	                src: 'src/js/*.js',
+	                dest: 'js/concat.js'
+	            },
+	            css: {
+	                src: 'src/css/*.css',
+	                dest: 'css/concat.css'
+	            }
+	        },
+	        cssmin: {
+	            css: {
+	                src: 'css/concat.css',
+	                dest: 'minified/css/concat.min.css'
+	            }
+	        },
+	        // Minifies JS files
+	        uglify: {
+	            options: {
+	                preserveComments: /^!|@preserve|@license|@cc_on/i,
+	                sourceMap: true,
+	                footer: '\n'
+	            },
+	            dist: {
+	                files: [
+	                    {
+	                        expand: true,
+	                        cwd: 'js',
+	                        src: 'concat.js',
+	                        dest: 'minified/js',
+	                        ext: '.min.js',
+	                        extDot: 'last'
+	                    }
+	                ]
+	            }
+	        },
+	        insert: {
+	            options: {},
+	            main: [
+	                {
+	                    src: "src/html/aviaForm.html",
+	                    dest: "src/html/Index",
+	                    match: "<!--avia-form-->"
+	                }, {
+	                    src: "src/html/railForm.html",
+	                    dest: "src/html/Index",
+	                    match: "<!--rail-form-->"
+	                }
+	            ],
+	            copy: {
+	                main: {
+	                    files: [
+	                        { expand: true, cwd: 'src/css/', src: '**', dest: 'origin/css/' },
+	                        { expand: true, cwd: 'src/js/', src: '**', dest: 'origin/js/' },
+	                        { expand: true, cwd: 'src/html/', src: '**', dest: 'html/' },
+	                        { expand: true, cwd: 'src/img/', src: '**', dest: 'img/' }
+	                    ],
+	                },
+	            }
+	        });
 
-		copy: {
-			main: {
-    			files: [      
-      			{expand: true, cwd:'src/css/',src: '**', dest: 'origin/css/'},
-      			{expand: true, cwd:'src/js/',src: '**', dest: 'origin/js/'},
-      			{expand: true, cwd:'src/html/',src:'**', dest: 'html/'},
-      			{expand: true, cwd:'src/img/',src:'**', dest: 'img/'}
-      			],
-      		},
-      	}
-	});	
+	    // Load tasks
+	    grunt.loadNpmTasks('grunt-contrib-concat');
+	    grunt.loadNpmTasks('grunt-css');
+	    grunt.loadNpmTasks('grunt-contrib-uglify');
+	    grunt.loadNpmTasks('grunt-githooks');
+	    grunt.loadNpmTasks('grunt-contrib-copy');
+	    grunt.loadNpmTasks('grunt-insert');
 
-	// Load tasks
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-css');
-	grunt.loadNpmTasks('grunt-contrib-uglify');	
-	grunt.loadNpmTasks('grunt-githooks');
-	grunt.loadNpmTasks('grunt-contrib-copy');		
-	
-	grunt.registerTask('default', ['concat','uglify','cssmin','copy']);
-	
 
-};
+	    grunt.registerTask('default', ['concat', 'uglify', 'cssmin', 'copy', 'insert']);
+
+
+	};
