61a3f4746f3984a678c4bb497118da2ba486dd42
Oles <boiaryn.o@gmail.com>
2017-10-05 15:31:15 +0300

    no message

--- push.sh
+++ push.sh
@@ -14,9 +14,9 @@ commit_website_files() {
  upload_files() {
    git remote add rezon https://${GH_TOKEN}@github.com/rezon-universal/form.git > /dev/null 2>&1           
    git fetch --quiet rezon    
-   #git checkout -b master   
+   git checkout -b master   
    #git merge minify --ff-only --quiet --commit -m "Travis build: $TRAVIS_BUILD_NUMBER" -X theirs   
-   git merge --allow-unrelated-histories rezon/master   
+   git merge --allow-unrelated-histories minify   
    git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
    git push -u -f --quiet rezon 
  }
